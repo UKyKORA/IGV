c04487c6af603c917625d0eb5f0c4368f4f19355
Matthew Russell <mbr4477@gmail.com>
2019-10-14 02:22:00 +0100

    Remove unneeded code in GPS

--- src/gps.py
+++ src/gps.py
@@ -7,20 +7,6 @@ import sys
 
 PORT = '/dev/ttyACM0'
 
-def set_speeds(ser, left, right):
-	#print 'Setting speeds ({0}, {1})'.format(int(left), int(right))
-	ser.write(str.encode('M1: {0}\r\nM2: {1}\r\n'.format(int(left), -int(right))))
-
-def ramp(ser, initial, final):
-	STEPS = 8
-	DT = 0.25
-	DS_LEFT = float(final[0] - initial[0]) / STEPS
-	DS_RIGHT = float(final[1] - initial[1]) / STEPS
-	for i in range(STEPS):
-		set_speeds(ser, initial[0] + DS_LEFT * i, initial[1] + DS_RIGHT * i)
-		time.sleep(DT)
-	set_speeds(ser, final[0], final[1])
-
 def parse_gpgga(line):
 	parts = line.split(',')
 	lat = parts[2]
