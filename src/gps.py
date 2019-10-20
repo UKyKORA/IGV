c7864fd987fe18202ed197d7eaa56687c7172a5c
Matthew Russell <mbr4477@gmail.com>
2019-10-20 18:36:54 -0400

    Add ros publishing to GPS node

--- src/gps.py
+++ src/gps.py
@@ -1,30 +1,40 @@
 #! /usr/bin/env python
 
-#import rospy
+import rospy
 import serial
 import time
 import sys
+from sensor_msgs.msgs import NavSatFix
 
+# NOTE: This will need changed depending on the actual GPS tty
 PORT = '/dev/ttyACM0'
 
 def parse_gpgga(line):
 	parts = line.split(',')
 	lat = parts[2]
-	lat_sign = (parts[3] == 'S') * -2 + 1
 	decimal_lat = float(lat[:2]) + float(lat[2:])/60
+	if parts[3] == 'S':
+		decimal_lat = -decimal_lat
 	lon = parts[4]
-	lon_sign = (parts[5] == 'W') * -2 + 1
 	decimal_lon = float(lon[:3]) + float(lon[3:])/60
-	return (decimal_lat * lat_sign, decimal_lon * lon_sign)
+	if parts[5] == 'W':
+		decimal_lon = -decimal_lon
+	msg = NavSatFix()
+	msg.latitude = decimal_lat
+	msg.longitude = decimal_lon
+	return msg
 
 def main():
-#	rospy.init_node('gps_node')
+	pub = rospy.Publisher('location', NavSatFix, queue_size=10)
+	rospy.init_node('gps_node')
+	rate = rospy.Rate(10) # 10Hz
 	ser = serial.Serial(PORT)
-	while True:
+	while not rospy.is_shutdown():
 		line = ser.readline()
 		if line.startswith('$GPGGA'):
-			print parse_gpgga(line)
-#	rospy.spin()
+			fix = parse_gpgga(line)
+			pub.publish(fix)
+		rate.sleep()
 
 # kick it off
 main()
