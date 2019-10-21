d0e765ed9a479bd171b71a8d95f1d707a78fdce2
Isaac Rowe <irowe.bbn@gmail.com>
2019-10-21 16:29:07 -0400

    Add ROS subscription to Motor controller node

--- src/motor_ctrl.py
+++ src/motor_ctrl.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import rospy
+from std_msgs.msg import motor_speeds
 import serial
 import time
 import sys
@@ -8,33 +9,22 @@ import sys
 DURATION = 5
 SPEED = -1023
 PORT = '/dev/ttyACM0'
+ser = serial.Serial(PORT)
 
 def set_speeds(ser, left, right):
 	#print 'Setting speeds ({0}, {1})'.format(int(left), int(right))
 	ser.write(str.encode('M1: {0}\r\nM2: {1}\r\n'.format(int(left), -int(right))))
 
-def ramp(ser, initial, final):
-	STEPS = 8
-	DT = 0.25
-	DS_LEFT = float(final[0] - initial[0]) / STEPS
-	DS_RIGHT = float(final[1] - initial[1]) / STEPS
-	for i in range(STEPS):
-		set_speeds(ser, initial[0] + DS_LEFT * i, initial[1] + DS_RIGHT * i)
-		time.sleep(DT)
-	set_speeds(ser, final[0], final[1])
+def motor_speed_callback(motor_speeds):
+	rospy.loginfo(rospy.get_caller_id() + "Try to set left motor to %s rad/s, right motor to %s rad/s", motor_speeds.left_motor_speed, motor_speeds.right_motor_speed)
+	set_speeds(ser, motor_speeds.left_motor_speed, motor_speeds.right_motor_speed);
 
 def main():
 	rospy.init_node('motor_ctrl_node')
-	print "\n\nSabertooth 2x32 Driver\n\n"
-	ser = serial.Serial(PORT)
-	ramp(ser, (0,0), (SPEED, SPEED))
-	time.sleep(DURATION)
-	ramp(ser, (SPEED, SPEED), (0, 0))
-	time.sleep(5)
-	ramp(ser, (0,0), (-SPEED, -SPEED))
-	time.sleep(DURATION)
-	ramp(ser, (-SPEED, -SPEED), (0,0))
-	rospy.spin()
+	rospy.Subscriber("motor_speeds", motor_speeds, motor_speed_callback)
 
-# kick it off
-main()
+	rospy.spin() # Keeps python from exiting while node is running
+
+if __name__ == '__main__':
+	# kick it off
+	main()
