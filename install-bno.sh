7a88e70380e5dd59bf030a0e2a64b666a227b67a
Matthew Russell <mbr4477@gmail.com>
2019-11-21 14:23:21 -0500

    Initial assets commit

--- install-bno.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y build-essential python-dev python-smbus python-pip git
-pushd ~
-git clone https://github.com/adafruit/Adafruit_Python_BNO055.git
-cd Adafruit_Python_BNO055
-sudo python setup.py install
-popd
--- src/config_loader.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-def load_yaml_config(filename):
-    cfg = None
-    with open(filename, 'r') as config_file:
-        cfg = yaml.load(config_file)
-    return cfg
